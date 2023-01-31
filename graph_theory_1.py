88712efa3a0b0d6c6e4ab08800725a56d1f43f60
intotito <intotito@gmail.com>
2023-01-31 23:41:58 +0000

    added counting variable passed by reference (passing a list instead of an int)

--- graph_theory_1.py
+++ graph_theory_1.py
@@ -1,29 +1,33 @@
 import math
-def quickSort(list: list, start: int, stop: int) -> list:
+
+count = [0]
+
+def quickSort(list: list, start: int, stop: int, count) -> list:
     i_pivot = stop
     pivot = list[i_pivot]
     i = start
-    count = 0
     while(i < i_pivot):
-        count += 1
+        count[0] += 1
         temp = list[i]
+   #     print('count = ', count[0])
+
     #    print('XXXXXX', 'i=', i, 'list[i]=', list[i])
         if(list[i] > pivot):
-            print('i=', i, 'i_pivot=', i_pivot, 'pivot=', pivot, 'list[i]=', list[i])
+      #      print('i=', i, 'i_pivot=', i_pivot, 'pivot=', pivot, 'list[i]=', list[i])
             list[i] = list[i_pivot - 1]
             list[i_pivot] = temp
             i_pivot -= 1
             list[i_pivot] = pivot
         else:
             i += 1
-            print('increment at', i)
+       #     print('increment at', i)
     if((i_pivot - 1) - start > 0):
-        quickSort(list, start, i_pivot - 1)
+        quickSort(list, start, i_pivot - 1, count)
     if(stop - (i_pivot + 1) > 0):
-        quickSort(list, i_pivot + 1, stop)
-    print('Count =', count, 'i_pivot=', i_pivot, 'i=', i)
+        quickSort(list, i_pivot + 1, stop, count)
+ #   print('Count =', count, 'i_pivot=', i_pivot, 'i=', i)
     return list
 
-list =[2, 43, 52, -4, -5, 34, 8, 23, 14, 16, 4, 3, 1, -5, 0, -88]
-
-print(quickSort(list, 0, len(list) - 1))
+list =[2, 43, 52, -4, -5, 34, 8, 23, 14, 16, 4, 3, 1, -5, 0, 20]
+print(quickSort(list, 0, len(list) - 1, count))
+print('count = ', count[0])
